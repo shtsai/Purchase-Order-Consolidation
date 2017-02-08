e8c5fca17aabf0b69ef383bdb00cfb36b142a214
Shang-Hung Tsai <st3127@nyu.edu>
2017-02-08 03:49:37 -0500

    add new file name creator

--- gui.py
+++ gui.py
@@ -2,7 +2,14 @@ import tkinter
 from tkinter import filedialog
 
 root = tkinter.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename()
-print(file_path)
+root.withdraw()   # hide the gui form
 
+# open up a file dialog to obtain the file path
+file_path = filedialog.askopenfilename()
+
+index = file_path.find(".xlsx")
+if index == -1:
+    print("invalid file path: selected file is not an .xlsx file")
+else:
+    new_file_path = file_path[:index] + "-統一格式" + file_path[index:]
+    print(new_file_path)
