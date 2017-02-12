df8826e5f9e8c2cdd3466f6d11a416572c9a5c80
Shang-Hung Tsai <shtsai7@gmail.com>
2017-02-12 04:01:51 -0500

    update rakuten, change new file path

--- rakuten/gui.py
+++ rakuten/gui.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import filedialog
+import os
 
 def get_file_path():
     root = tkinter.Tk()
@@ -17,12 +18,20 @@ def get_file_path():
 
 
 def generate_new_filename(file_path, s):
+    if os.name == "nt":
+        slash = "\\"
+    else:
+        slash = "/"
+
     index = file_path.find(".xlsx")
     if index == -1:
         print("invalid file path: selected file is not an .xlsx file")
         return -1
     else:
         new_file_path = file_path[:index] + "-" + s + file_path[index:]
+        filename = os.path.basename(new_file_path)
+        new_file_path = os.getcwd() + slash + filename
+
         return new_file_path
 
 
